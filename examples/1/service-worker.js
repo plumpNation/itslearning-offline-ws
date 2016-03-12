2b7d7558e57abd30dcf48428c5d3cb9ee6e9fd54
Gavin King <gavin.king@itslearning.com>
2016-03-12 14:36:35 +0100

    Adding more ways to experiment with the lifecycle of the service worker

--- examples/1/service-worker.js
+++ examples/1/service-worker.js
@@ -6,12 +6,22 @@
 
 // Adjust this version and watch the effect it has on the workers when you refresh, then
 // close the browser tab.
-var version = 'v1-lifecycle-example';
+var version = 'v2-lifecycle-example';
 
 console.log('Running Service Worker', version);
 
 self.addEventListener('install', function (event) {
     console.info(version, 'installing');
+
+    // to see the install stage in the dev tools
+    // var aTimeHasPassed = delayBy(5);
+    // event.waitUntil(aTimeHasPassed);
+
+    // To activate the service worker immediately...
+    // self.skipWaiting();
+
+    // to force `redundant` state
+    // throw new Error('SMASH');
 });
 
 self.addEventListener('activate', function (event) {
@@ -21,3 +31,16 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
     console.info(version, 'requesting', event.request.url);
 });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////  HELPERS  //////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function delayBy(seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log('boom');
+            resolve();
+        }, seconds * 1000);
+    });
+}

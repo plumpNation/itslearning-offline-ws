925bc406556d53d79233c5b8f28bbb16c1851451
Gavin King <gavin.king@itslearning.com>
2016-03-18 17:12:40 +0100

    Working POST

--- workshop/6/start/lib/components/news-form.js
+++ workshop/6/start/lib/components/news-form.js
@@ -14,40 +14,55 @@
 
     function render() {
 
-        let template =
-            `<form id="news-form" class="pure-form pure-form-aligned">
-                <fieldset class="pure-group">
-                    <input
-                        name="headline"
-                        class="pure-input-1-2"
-                        placeholder="Headline">
+        let element,
+            template =
+                `<form
+                    id="news-form"
+                    method="post"
+                    action="./news.json"
+                    class="pure-form pure-form-aligned">
 
-                    <textarea
-                        name="body"
-                        class="pure-input-1-2"
-                        placeholder="Body">
-                    </textarea>
-                </fieldset>
+                    <fieldset class="pure-group">
+                        <input
+                            name="headline"
+                            class="pure-input-1-2"
+                            placeholder="Headline">
 
-                <fieldset class="pure-group">
-                    <input name="author" placeholder="Author">
-                    <select name="avatar">
-                        <option>ericf</option>
-                        <option>andrew</option>
-                        <option>reid</option>
-                        <option>tilo</option>
-                        <option>gavin</option>
-                    </select>
-                </fieldset>
+                        <textarea
+                            name="body"
+                            class="pure-input-1-2"
+                            placeholder="Body"></textarea>
+                    </fieldset>
 
-                <button type="submit" class="pure-button pure-button-primary">Submit</button>
-            </form>`;
+                    <fieldset class="pure-group">
+                        <input name="author" placeholder="Author">
+                        <select name="avatar">
+                            <option>ericf</option>
+                            <option>andrew</option>
+                            <option>reid</option>
+                            <option>tilo</option>
+                            <option>gavin</option>
+                        </select>
+                    </fieldset>
+
+                    <button type="submit" class="pure-button pure-button-primary">Submit</button>
+                </form>`;
+
 
         document.body.insertAdjacentHTML('beforeend', template);
 
-        document.getElementById('news-form').addEventListener('submit', (event) => {
+        $('[name="headline"]').focus();
+
+        element = $('form#news-form');
+
+        element.on('submit', (event) => {
             event.preventDefault();
-            console.log('submitting');
+
+            $.post('./news.json', element.serialize(), (response) => {
+                console.log(response);
+            });
+
+            element.remove();
         });
     }
 
--- workshop/6/start/server.py
+++ workshop/6/start/server.py
@@ -3,6 +3,7 @@ import SocketServer
 import json
 import logging
 import sys
+from urlparse import parse_qs
 
 database = json.loads(open('database.json').read());
 
@@ -12,42 +13,46 @@ else:
     port = 8000
 
 class Handler(SimpleHTTPRequestHandler):
-    def sendDatabase(self):
+    def sendDatabaseJSON(self):
         response = json.dumps(database)
 
         self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.send_header("Content-length", len(response))
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-length', len(response))
         self.end_headers()
         self.wfile.write(response)
 
     def do_GET(self):
 
         if (self.path.endswith('news.json')):
-            self.sendDatabase()
+            self.sendDatabaseJSON()
 
         else:
             SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
         if (self.path.endswith('news.json')):
+
+            length = int(self.headers['content-length'])
+            postvars = parse_qs(self.rfile.read(length), keep_blank_values=1)
+
             database['news'].insert(0, {
-                "headline": "This is your data",
-                "body": "It comes from a flat file in the home folder",
-                "author": "Gavin King",
-                "avatar": "gavin"
+                'headline': postvars['headline'][0],
+                'body': postvars['body'][0],
+                'author': postvars['author'][0],
+                'avatar': postvars['avatar'][0]
             });
 
-            self.sendDatabase()
+            self.sendDatabaseJSON()
 
         else:
             SimpleHTTPRequestHandler.do_GET(self)
 
 SocketServer.TCPServer.allow_reuse_address=True
-httpd = SocketServer.TCPServer(("0.0.0.0", port), Handler)
+httpd = SocketServer.TCPServer(('0.0.0.0', port), Handler)
 
 sa = httpd.socket.getsockname()
 
-print "Serving HTTP on", sa[0], "port", sa[1], "..."
+print 'Serving HTTP on', sa[0], 'port', sa[1], '...'
 
 httpd.serve_forever()

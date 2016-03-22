77b42e79749045d51941c17d3e2e2729d2ce6593
Gavin King <gavin.king@itslearning.com>
2016-03-22 16:12:29 +0100

    Adding commenting in the finished example

--- workshop/3/finished/service-worker.js
+++ workshop/3/finished/service-worker.js
@@ -1,11 +1,3 @@
-/**
- * A service worker must be directly on the scope.
- * The service worker will only catch requests from clients under the service worker's scope.
- * The max scope for a service worker is the location of the worker.
- */
-
-// Adjust this version and watch the effect it has on the workers when you refresh, then
-// close the browser tab.
 let version = 'v1-cache-api';
 
 console.info('Executing service worker for', version);
@@ -25,23 +17,33 @@ self.addEventListener('fetch', (event) => {
         return;
     }
 
+    // Since we are going to take over the request/response by calling
+    // event.respondWith, we must manually fetch the request.
     let fetchedNews =
+
+            // Use a clone of the request object, so we can use it again.
             fetch(event.request.clone())
+
+                // fetch returns a Promise
                 .then(function (response) {
 
+                    // Use a clone of the response object as we will use it again
+                    // in `event.respondWith()`
                     cacheResponse(event.request, response.clone());
 
+                    // So the end result of this fetch then chain is that fetchedNews will be
+                    // a Promise that resolves to this response;
                     return response;
                 });
 
-    // fetch the request, cache it and respond with the response
-
+    // NOTE: Called synchronously
     event.respondWith(fetchedNews);
 });
 
 function cacheResponse(request, response) {
     caches.open(version)
         .then(function (cache) {
+            console.info('Caching -->', request.url, '<--');
             cache.put(request, response);
         });
 }

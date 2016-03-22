f691eae3c5f6e60c227f066e72c6d43d407a3a34
Gavin King <gavin.king@itslearning.com>
2016-03-22 16:08:55 +0100

    Tidying up code

--- workshop/3/start/service-worker.js
+++ workshop/3/start/service-worker.js
@@ -1,11 +1,3 @@
-/**
- * A service worker must be directly on the scope.
- * The service worker will only catch requests from clients under the service worker's scope.
- * The max scope for a service worker is the location of the worker.
- */
-
-// Adjust this version and watch the effect it has on the workers when you refresh, then
-// close the browser tab.
 let version = 'v1-cache-api';
 
 console.info('Executing service worker for', version);
